7f5aed6846a896ff1315e6b861d28411bf22ce03
Madonahs <syombuamadona@gmail.com>
2019-06-04 13:42:29 -0500

    added author

--- Lesson3/myapi/app/src/main/java/com/madonasyombua/myapplication/MainActivity.kt
+++ Lesson3/myapi/app/src/main/java/com/madonasyombua/myapplication/MainActivity.kt
@@ -3,6 +3,10 @@ package com.madonasyombua.myapplication
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * @author
+ */
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
