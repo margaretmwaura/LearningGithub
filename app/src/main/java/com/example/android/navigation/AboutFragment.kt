743e56953635706e2f9351fab67791585ae5e83f
Madonahs <syombuamadona@gmail.com>
2019-06-04 13:19:05 -0500

    l1

--- app/src/main/java/com/example/android/navigation/AboutFragment.kt
+++ app/src/main/java/com/example/android/navigation/AboutFragment.kt
@@ -21,7 +21,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+//@author
 class AboutFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
